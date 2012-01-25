91d07a2a4b5e7e804665c675c7ed81270ff5513a
Riley Lynch <bx+github@teleological.net>
2012-01-25 13:58:40 -0500

    Added Bulgarian messaging, (thanks, Ivan Cenov!) version bump: v0.2.1

--- init.rb
+++ init.rb
@@ -10,7 +10,7 @@ Redmine::Plugin.register :redmine_stealth do
   author      'Riley Lynch'
   description 'Enables users to disable Redmine email notifications ' +
               'for their actions'
-  version     '0.2.0'
+  version     '0.2.1'
 
   if respond_to?(:url)
     url 'http://teleological.github.com/redmine-stealth-plugin'
