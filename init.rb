283b92d033ab25c8603fd8b429145eba8d2a20fc
Riley Lynch <bx+github@teleological.net>
2012-02-16 15:05:10 -0500

    Added German messaging by Danny Schäfer, version bump: v0.2.2

--- init.rb
+++ init.rb
@@ -10,7 +10,7 @@ Redmine::Plugin.register :redmine_stealth do
   author      'Riley Lynch'
   description 'Enables users to disable Redmine email notifications ' +
               'for their actions'
-  version     '0.2.1'
+  version     '0.2.2'
 
   if respond_to?(:url)
     url 'http://teleological.github.com/redmine-stealth-plugin'
